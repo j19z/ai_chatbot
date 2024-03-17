26d2b8e97beec7b6a5b336e76117e44cfb2e6e5a
J19Z <jgmusso@gmail.com>
2024-03-17 13:20:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Simple chatbot using a Hugging Face LLM API and Streamlit.
+
+Model Mistal: 
+https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2
+
+The chatbot response is very limited since im usuing the API. All API requests are made directly to Hugging Face free server. 
+
+ 
---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import streamlit as st
+import requests
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
+headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
+
+def generate_response(prompt):
+    API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+        
+    output = query({
+        "inputs": f"{prompt}",
+    })
+
+    try:
+        generated_text = output[0]['generated_text']
+    except KeyError:
+        print('KeyError encountered when accessing "generated_text"')
+        generated_text = 'KeyError encountered when accessing "generated_text"'
+    
+    return generated_text.replace(prompt, "").strip()
+
+#Streamlit
+messages = st.container(height=600)
+if prompt := st.chat_input("Say something"):
+    messages.chat_message("user").write(prompt)
+    response = generate_response(prompt)
+    messages.chat_message("assistant").write(f"AiChat: {response}")
